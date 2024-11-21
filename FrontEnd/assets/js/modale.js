9df5065af1bd7fadfb27236cf2270410a1363a65
Nolwenn PENNEC CHARLES <nolwenn.pennec@hotmail.com>
2024-11-21 20:27:26 +0100

    début modale - jusqu'à impossible de fermer de l'interieur

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -1,19 +1,36 @@
 //*************************************************************************************************************************/
 //**************************************************blabla **************************************************/
 //*************************************************************************************************************************/
+let modale = null
 
-const modale = document.querySelector("#modale")
-const lienModale = document.querySelector(".lien-modale")
+const ouvertureModale = function (e) {
+    e.preventDefault() 
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display = null 
+    target.removeAttribute("aria-hidden")
+    target.setAttribute("aria-modal","true")
+    modale = target
+    modale.addEventListener('click', fermetureModale)
+    modale.querySelector('.fermer-modale').addEventListener('click', fermetureModale)
+    modale.querySelector('.js-modale-stop').addEventListener('click', stopPropagation)
+}
 
-lienModale.addEventListener ("click", () => {
+const fermetureModale = function (e) {
+    if (modale === null) return
+    e.preventDefault() 
+    modale.style.display = "none" 
+    modale.setAttribute("aria-hidden","true")
+    modale.removeAttribute("aria-modal")
+    modale.removeEventListener('click', fermetureModale)
+    modale.querySelector('.fermer-modale').removeEventListener('click', fermetureModale)
+    modale.querySelector('.js-modale-stop').removeEventListener('click', stopPropagation)
+    modale = null
+}
 
-    let modaleContenuGalleyPhoto = `
-        <aside id="modale" class="modal js-modal" aria-hidden="true" aria-labelledby="title_modal">
-            <div class="modale-conteneur">
-                <i class="fa-solid fa-xmark modale-fermer"></i>
-                <h3 id="modale-titre">Galerie Photos</h3>
-            <div class="modale-gallerie" id="modaleGallerie">
-        </aside>
-    `
-    modale.insertAdjacentHTML('beforeend', modaleContenuGalleyPhoto)
+const stopPropagation = function (e){
+    e.stopPropagation()
+}
+
+document.querySelectorAll('.js-modale').forEach(a => {
+    a.addEventListener('click', ouvertureModale)
 })
