abd831f69aa19a97866a8216d108732b3a36b1d2
Nolwenn PENNEC CHARLES <nolwenn.pennec@hotmail.com>
2024-11-21 21:07:27 +0100

    fermeture modale terminée

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -2,14 +2,18 @@
 //**************************************************blabla **************************************************/
 //*************************************************************************************************************************/
 let modale = null
+const selectionFocusable = 'button, a, input, textarea'
+let focusables = []
 
 const ouvertureModale = function (e) {
     e.preventDefault() 
-    const target = document.querySelector(e.target.getAttribute('href'))
-    target.style.display = null 
-    target.removeAttribute("aria-hidden")
-    target.setAttribute("aria-modal","true")
-    modale = target
+    modale = document.querySelector(e.target.getAttribute('href'))
+    focusables = Array.from(modale.querySelectorAll(selectionFocusable))
+    focusables[0].focus()
+    modale.style.display = null 
+    modale.removeAttribute("aria-hidden")
+    modale.setAttribute("aria-modal","true")
+    
     modale.addEventListener('click', fermetureModale)
     modale.querySelector('.fermer-modale').addEventListener('click', fermetureModale)
     modale.querySelector('.js-modale-stop').addEventListener('click', stopPropagation)
@@ -31,6 +35,33 @@ const stopPropagation = function (e){
     e.stopPropagation()
 }
 
+
+const focusModale = function(e) {
+    e.preventDefault()
+    let index = focusables.findIndex(f => f === modale.querySelector(':focus'))
+    if (e.shift === true){
+        index--
+    } else{
+        index ++  
+    }
+    if (index >= focusables.length) {
+        index = 0
+    }
+    if (index <0) {
+        index = focusables.length - 1
+    }
+    focusables[index].focus()
+}
+
 document.querySelectorAll('.js-modale').forEach(a => {
     a.addEventListener('click', ouvertureModale)
+})
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        fermetureModale(e)
+    }
+    if (e.key ==="Tab" && modale !==null) {
+        focusModale(e)
+    }
 })
